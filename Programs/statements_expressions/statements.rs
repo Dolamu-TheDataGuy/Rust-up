e0d3472c2192ea9d651685c1678a70937fdffa9b
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-04 21:15:02 +0100

    added statement + functions

--- Programs/statements_expressions/statements.rs
+++ Programs/statements_expressions/statements.rs
@@ -5,24 +5,47 @@
 
 // Example 1
 
+// fn main() {
+//     let x: u32 = 5;
+
+//     let y: u32 = {
+//         let x_squared = x * x;
+//         let x_cubed = x_squared * x;
+
+//         // This expression will be assigned to `y`
+//         x_cubed + x_squared + x
+//     };
+
+//     let z: u32 = {
+//         // The semicolon suppresses this expressions and `()` is assigned to `z`
+//         2 * x
+//     };
+
+//     print!("x is {:?}\n", x);
+//     print!("y is {:?}\n", y);
+//     print!("z is {:?}\n", z);
+
+// }
+
+
+// fn main() {
+//     let v: i32 = {
+//         let mut x: i32 = 1;
+//         x += 2; // 3 but this is a statement
+//         x
+//     };
+
+//     assert_eq!(v, 3);
+
+//     print!("Success!\n");
+// }
+
 fn main() {
-    let x: u32 = 5;
+    let s = sum(1, 2); // 3
+    assert_eq!(s, 3);
+    print!("Success!\n");
+}
 
-    let y = {
-        let x_squared = x * x;
-        let x_cubed = x_squared * x;
-
-        // This expression will be assigned to `y`
-        x_cubed + x_squared + x
-    };
-
-    let z = {
-        // The semicolon suppresses this expressions and `()` is assigned to `z`
-        2 * x;
-    };
-
-    print!("x is {:?}", x);
-    print!("y is {:?}", y);
-    print!("z is {:?}", z);
-
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
 }
