9aa1707b1b5419bb8a46a68cd655dcce8961966c
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-04 17:00:16 +0100

    size of character

--- Programs/rust_variables/data_types.rs
+++ Programs/rust_variables/data_types.rs
@@ -171,5 +171,15 @@ fn main() {
  }
 
 // Char, Bool, and Unit.
+//1. Character (Char)
+use std::mem::size_of_val;
+fn main() {
+    let c1 = 'a';
+    assert_eq!(size_of_val(&c1),1);
 
+    let c2 = 'b';
+    assert_eq!(size_of_val(&c2), 3);
+
+    print!("Success!");
+}
 
