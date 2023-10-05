129f3250fb7d77539ba68baf039f74e3d7c1e33c
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-05 17:53:00 +0100

    reverted changes

--- Programs/Ownership/clone.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    //use as many approaches as you can to make it work
-    let x: String = String::from("hello, world");
-    let y: String = x.clone();
-    println!("{}, {}", x, y); //trying to access x after it has been assigned to y, x has been damaged after assigning it to y
-} // we have to deepcopy x by using the clone method
