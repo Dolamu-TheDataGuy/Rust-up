534d76e521b73c57b0c50a15e6c9169cfea7e2a4
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-06 06:46:42 +0100

    added reference borrowing

--- Programs/borrowing/ref_borrow.rs
+++ Programs/borrowing/ref_borrow.rs
@@ -59,3 +59,25 @@ fn main() {
     println!("Success!");
 }
 
+// 6. Ref: ref can be used to take references to a value, similar to &.
+
+fn main() {
+    let c: char = 'a';
+
+    let r1: &char = &c;
+
+    // Fill the blank, don't change other code
+    let ref r2 = c;
+
+    assert_eq!(*r1, *r2);
+
+    // Check the equality of the two address strings
+    assert_eq!(get_addr(r1), get_addr(r2));
+
+    print!("Success!");
+}
+
+// Get memory address string
+fn get_addr(r: &char) -> String {
+    format!("{:p}", r)
+}
