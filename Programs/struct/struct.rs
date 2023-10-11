795f8558235c741fabea09250ef1080bf5e4e528
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-11 20:24:30 +0100

    added template struct in rust

---
+++ Programs/struct/struct.rs
@@ -0,0 +1,27 @@
+// STRUCT
+// * compound type allowing to group together values of different types into
+// a named data structure.
+
+// Similar to tuples, but each value has a name so values can be accessed
+// through his name.
+
+// Have to be instanciated with data, think of it like the struct is the
+// template for the instances you create from it.
+
+// Template
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    // Instance of struct User
+    let user1 = User {
+        active: true,
+        username: String::from("Dolamu"),
+        email: String::from("oludaredolamu@gmail.com"),
+        sign_in_count: 1,
+    };
+}
