6950f3a38e85787c6204d98f86ee167018dba9df
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-10-12 10:45:34 +0100

    added code to loop through array and corrected ownership bug

--- Programs/control_flow/if_else.rs
+++ Programs/control_flow/if_else.rs
@@ -1,5 +1,5 @@
 #![allow(warnings)]
-
+// 1.
 // fn main() {
 //     let n: i32 = 5;
 
@@ -14,17 +14,48 @@
 
 // 2 . if/else expressions can be used in assignments.
 
+// fn main() {
+//     let n: i32 = 5;
+
+//     let big_n = 
+//         if n < 10 && n > -10 {
+//             println!(", and is a small number, increase by ten fold");
+//             10 * n
+//         } else {
+//             println!(", and is a big number, halve the number");
+
+//             n / 2.0 as i32
+//         };
+//     print!("{} -> {}\n", n, big_n);
+// }
+
+// 3. The for in construct can be used to iterate through an iterator, e.g a range
+// 
+
+// fn main() {
+// for n in 1..100 {// modify this line to make the code work
+//     if n == 100 {
+//         panic!("NEVER LET THIS RUN")
+//     }
+// }
+//     print!("Success!");
+// }
+
+
+// 4 . 
+// Fix the errors without adding or removing lines
 fn main() {
-    let n: i32 = 5;
+    let names: [String; 2] = [String::from("liming"), String::from("hanmeime")];
+    for name in &names {
+        print!("{}\n", name)
+    }
 
-    let big_n = 
-        if n < 10 && n > -10 {
-            println!(", and is a small number, increase by ten fold");
-            10 * n
-        } else {
-            println!(", and is a big number, halve the number");
+    println!("{:?}", names); // violate ownership rule.
 
-            n / 2.0 as i32
-        };
-    print!("{} -> {}\n", n, big_n);
+    let numbers: [i32; 3] = [1, 2, 3];
+    for mut number in numbers {
+        number = number * 2;
+        print!("{}\n", number);
+    }
+    print!("{:?}\n", numbers);
 }
