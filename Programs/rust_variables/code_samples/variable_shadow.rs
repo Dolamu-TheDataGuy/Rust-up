cc395146601ec7ad25b38a86d2ec10b4a83c8821
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-08 09:20:33 +0100

    added shadowing and mutability code

--- Programs/rust_variables/code_samples/variable_shadow.rs
+++ Programs/rust_variables/code_samples/variable_shadow.rs
@@ -1,15 +1,29 @@
 // Shadowing allows you to re-initialize a variable with another value.
+#![allow(warnings)]
+// fn main() {
+//     let x: i32 = 5;
+
+//     {
+//          let x = 12;
+//          assert_eq!(x, 12);
+//     }
+
+//     assert_eq!(x, 5);
+
+//     let x: i32 = 42; // shadowing variable x
+//     println!("{}", x) //Prints 42
+// }
 
 fn main() {
-    let x: i32 = 5;
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    let mut x = x;
+    x += 3;
+    
 
-    {
-         let x = 12;
-         assert_eq!(x, 12);
-    }
-
-    assert_eq!(x, 5);
-
-    let x: i32 = 42; // shadowing variable x
-    println!("{}", x) //Prints 42
-}
+    let y = 4;
+    // Shadowing
+    let y: &str = "I can also  be bound to a text";
+    println!("{}\n {}", x, y);
+} 
