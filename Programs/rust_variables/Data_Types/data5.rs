536783729200933c78b4a56b36ee5115e8ceda6d
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-08 23:40:06 +0100

    added solution to data type problem

--- Programs/rust_variables/Data_Types/data5.rs
+++ Programs/rust_variables/Data_Types/data5.rs
@@ -2,8 +2,8 @@
 #[allow(unused_variables)]
 
 fn main() {
-    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
-    assert!(v == 1579);
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111; // 1024 + 255 + 63 + 255
+    assert!(v == 1597);
 
     println!("Success!");
 }
