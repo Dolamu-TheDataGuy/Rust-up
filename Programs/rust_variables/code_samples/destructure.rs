7dd12d40efd2f1a7854e82efc4f26dbfcf9ca98f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-08 10:31:17 +0100

    added code for destructuring

--- Programs/rust_variables/code_samples/destructure.rs
+++ Programs/rust_variables/code_samples/destructure.rs
@@ -1,8 +1,19 @@
-fn main() {
-    let (mut x, y) = (1,2);
-    x += 2;
+// fn main() {
+//     let (mut x, y) = (1,2);
+//     x += 2;
 
-    assert_eq!(x, 3);
-    assert_eq!(y, 2);
+//     assert_eq!(x, 3);
+//     assert_eq!(y, 2);
+//     println!("Success!");
+// }
+
+fn main() {
+    let (x, y);
+
+    (x, ..) = (3, 5);
+    [.., y] = [1, 2];
+
+    assert_eq!([x,y], [3,2]);
+
     println!("Success!");
 }
