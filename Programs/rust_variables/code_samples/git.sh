b59cb024413ecb83d4f7d224628b25a22f4ddc30
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-08 09:04:08 +0100

    added solution to the scope code

--- Programs/rust_variables/code_samples/git.sh
+++ Programs/rust_variables/code_samples/git.sh
@@ -14,3 +14,4 @@ read Branch_name
 
 git push $REMOTE $Branch_name
 
+
--- Programs/rust_variables/code_samples/variable_fix.rs
+++ Programs/rust_variables/code_samples/variable_fix.rs
@@ -1,10 +1,10 @@
 // Fix error with the use of define_x
 
 fn main() {
-    println!("{}, world", x);
-
+    define_x();
 }
 
 fn define_x() {
     let x: &str = "hello";
+    println!("{}, world", x);
 }
