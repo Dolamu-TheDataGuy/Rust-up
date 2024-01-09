e92c7e8f2cd139b1879fae70850ef903431f3d5f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-09 21:18:37 +0100

    added computation logic codes

---
+++ Programs/rust_variables/Data_Types/compute.rs
@@ -0,0 +1,26 @@
+#![allow(warnings)]
+#[allow(unused_variables)]
+
+fn main() {
+    // Integer addition
+    assert!(1u32 + 2 == 3);
+
+    // Integer subtraction
+    assert!(1i32 - 2i32 == -1i32);
+    assert!(1i8 - 2i8  == -1);
+
+
+    assert!(3 * 50 == 150)
+
+    assert!(9.6 / 3.2 == 3.0)
+
+    assert!(24 % 5 == 4);
+
+    //Short-circuiting boolean logic
+    assert!(true && false == __);
+    assert!(true || false == __);
+    assert!(!true == __);
+
+    //Bitwise operations
+
+}
