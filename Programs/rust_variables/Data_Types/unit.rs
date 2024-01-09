6f3e71c47a3f38744b0cd11a0b42fd27d2da3540
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-09 23:33:05 +0100

    added text for char, unit, bool

---
+++ Programs/rust_variables/Data_Types/unit.rs
@@ -0,0 +1,25 @@
+// Unit type in rust is like None in Python
+#![allow(warnings)]
+#[allow(unused_variables)]
+#[allow(unused_imports)]
+
+
+fn main() {
+    let v: () = ();
+
+    let x: (i32, i32) = (2,3);
+    assert_eq!(v, implicitly_ret_unit());
+
+    println!("Success!");
+
+}
+
+fn implicitly_ret_unit() {
+    println!("I will return a ()");
+}
+
+fn explicitly_ret_unit() {
+    println!("I will return a ()");
+}
+
+
