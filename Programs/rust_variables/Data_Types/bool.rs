d19d898a1e22390c4520544be46d3d190145b231
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-09 23:09:16 +0100

    added bool code

---
+++ Programs/rust_variables/Data_Types/bool.rs
@@ -0,0 +1,13 @@
+#![allow(warnings)]
+#[allow(unused_variables)]
+#[allow(unused_imports)]
+
+fn main() {
+    let f: bool = false;
+
+    let t: bool = false;
+
+    if !t {
+        println!("Success!");
+    }
+}
