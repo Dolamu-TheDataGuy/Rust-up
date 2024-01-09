2b082569e55b459ccced172825a6724553485343
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-09 21:25:51 +0100

    added boolean logic text

--- Programs/rust_variables/Data_Types/compute.rs
+++ Programs/rust_variables/Data_Types/compute.rs
@@ -10,17 +10,19 @@ fn main() {
     assert!(1i8 - 2i8  == -1);
 
 
-    assert!(3 * 50 == 150)
+    assert!(3 * 50 == 150) // i32
 
-    assert!(9.6 / 3.2 == 3.0)
+    assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32 )
 
     assert!(24 % 5 == 4);
-
+    
     //Short-circuiting boolean logic
-    assert!(true && false == __);
+    assert!(true && false == __); 
     assert!(true || false == __);
     assert!(!true == __);
 
     //Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+
 
 }
