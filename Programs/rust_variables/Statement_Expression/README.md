84a2471ad23203836574c6e064904f16b8600cc8
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-01-15 23:14:56 +0100

    added readme for statement and expression and code

---
+++ Programs/rust_variables/Statement_Expression/README.md
@@ -0,0 +1,10 @@
+# STATEMENTS & EXPRESSION IN RUST
+
+* Statement:
+    - Instructions that perform some action but do not produce a value
+
+    - Function definitions are statements, as well as code that ends with `;` (usually).
+
+
+* Expression:
+    - Evaluates to a resultant value and always lack the `;` symbol at the end.
---
+++ Programs/rust_variables/Statement_Expression/example1.rs
@@ -0,0 +1,21 @@
+#![allow(warnings)]
+#[allow(unused_variables)]
+#[allow(unused_imports)]
+
+fn main() {
+    let x: u32 = 5;
+
+    let y = {
+        let x_squared = x * x;
+
+        let x_cube = x_squared * x;
+
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x
+    }
+
+    let z = {
+        // The semicolon supresses this expression and `()` is assigned to `z`
+        2 * x;
+    }
+}
