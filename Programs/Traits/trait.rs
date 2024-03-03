55261519d699a809eeceddf208cb54c5efed39a5
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2024-03-03 23:53:52 +0100

    added code for traits

---
+++ Programs/Traits/trait.rs
@@ -0,0 +1,62 @@
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+  fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+
+
+impl Summary for Tweet {
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+
+
+fn main() {
+    let tweet: Tweet = Tweet {
+        username: String::from("@johndoe"),
+        content: String::from("The Sky is Falling"),
+        reply: false,
+        retweet: false
+    };
+
+    let article: NewsArticle = NewsArticle {
+        author: String::from("John Doe"),
+        headline: String::from("The Sky is Falling!"),
+        content: String::from("The sky is not actually falling.")
+    };
+
+    println!("Tweet summary: {}", tweet.summarize());
+    println!("Article summary: {}", article.summarize());
+}
